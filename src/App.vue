940f0d12b020e8c286dff923a1b695dc72d930e1
PSmigielski <walek.smigielski@gmail.com>
2020-06-29 11:42:33 +0200

    add working input component

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,41 @@
 <template>
   <div id="app">
     <h1>space photos search</h1>
-    <Search />
+    <Search v-model="inputValue" @input="handleInput" />
   </div>
 </template>
 
 <script>
+const debounce = require('lodash.debounce');
+
+import axios from 'axios';
+
+const Api = 'https://images-api.nasa.gov/search';
+
 import Search from '@/components/Search.vue';
 
 export default {
 	name: 'App',
 	components: {
 		Search,
+  },
+  	data() {
+		return {
+			inputValue: '',
+			results: [],
+		};
+	},
+	methods: {
+		// eslint-disable-next-line
+		handleInput: debounce(function () {
+			axios.get(`${Api}?q=${this.inputValue}&media_type=image`)
+				.then((response) => {
+					this.results = response.data.collection.items;
+					console.log(this.results);
+				}).catch((e) => {
+					console.log(e);
+				});
+		}, 500),
 	},
 };
 </script>
--- src/components/Search.vue
+++ src/components/Search.vue
@@ -1,37 +1,26 @@
 <template>
     <div class="searchWrapper">
         <label for="searchBar">search</label>
-        <input id="searchBar" name="search" v-model="inputValue" @input="handleInput">
+        <input id="searchBar" name="search" :value="props" @input="handleChange">
     </div>
 </template>
 
 <script>
-const debounce = require('lodash.debounce');
 
-import axios from 'axios';
-
-const Api = 'https://images-api.nasa.gov/search';
 
 export default {
 	name: 'Search',
-	data() {
-		return {
-			inputValue: '',
-			results: [],
-		};
+	props: {
+		value:{
+			type:String,
+			required:true,
+		}
 	},
-	methods: {
-		// eslint-disable-next-line
-		handleInput: debounce(function () {
-			axios.get(`${Api}?q=${this.inputValue}&media_type=image`)
-				.then((response) => {
-					this.results = response.data.collection.items;
-					console.log(this.results);
-				}).catch((e) => {
-					console.log(e);
-				});
-		}, 500),
-	},
+	methods:{
+		handleChange(e){
+			this.$emit('input', e.target.value);
+		}
+	}
 };
 </script>
 
